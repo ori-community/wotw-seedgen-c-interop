4a483b419dd68ee4a0d8a0f3425bb20348064309
SiriusAshling <53594055+SiriusAshling@users.noreply.github.com>
2023-02-13 12:10:33 +0100

    fun

---
+++ src/lib.rs
@@ -0,0 +1,110 @@
+use std::slice;
+use std::ffi::{CString, CStr};
+use std::panic;
+
+use cty::{c_char, c_int, c_void};
+
+use wotw_seedgen::settings::UniverseSettings;
+use wotw_seedgen::logic;
+use wotw_seedgen::Inventory;
+use wotw_seedgen::item::{Item, Skill, Teleporter, Shard};
+
+#[repr(C)]
+#[derive(Clone, Copy)]
+pub struct RawVec<T> {
+    data: *const T,
+    len: c_int,
+}
+
+#[repr(C)]
+pub struct Input {
+    skills: RawVec<c_int>,
+    teleporters: RawVec<c_int>,
+    shards: RawVec<c_int>,
+    nodes: RawVec<*const c_char>,
+    areas: *const c_char,
+    locations: *const c_char,
+    states: *const c_char,
+    seed_file: *const c_char,
+}
+pub type Data = *const c_void;
+#[repr(C)]
+pub struct Functions {
+    push_error: extern fn(data: Data, string: *const c_char),
+    push_reached: extern fn(data: Data, string: *const c_char),
+}
+impl Functions {
+    #[inline]
+    fn push_error(&self, data: Data, string: *const c_char) {
+        (self.push_error)(data, string)
+    }
+    #[inline]
+    fn push_reached(&self, data: Data, string: *const c_char) {
+        (self.push_reached)(data, string)
+    }
+}
+
+#[repr(C)]
+pub enum Status {
+    Success,
+    Error,
+    Panic,
+}
+
+fn handle_panics<F: FnOnce() -> Result<(), String> + panic::UnwindSafe>(data: Data, functions: &Functions, f: F) -> Status {
+    match panic::catch_unwind(f) {
+        Ok(result) => match result {
+            Ok(()) => Status::Success,
+            Err(err) => {
+                let error_message = CString::new(err).unwrap_or_else(|_| CString::new("Failed to convert error message").unwrap());
+                functions.push_error(data, error_message.into_raw());
+                Status::Error
+            }
+        },
+        Err(_) => Status::Panic,
+    }
+}
+
+unsafe fn convert_c_str<'a>(ptr: *const c_char) -> Result<&'a str, String> {
+    unsafe { CStr::from_ptr(ptr) }.to_str().map_err(|err| format!("invalid areas file: {err}"))
+}
+unsafe fn convert_c_vec<'a, T>(raw_vec: RawVec<T>) -> &'a [T] {
+    unsafe { slice::from_raw_parts(raw_vec.data, raw_vec.len as usize) }
+}
+
+#[no_mangle]
+pub extern "C" fn reach_check(input: &Input, data: Data, functions: &Functions) -> Status {
+    handle_panics(data, functions, || {
+        unsafe {
+            let mut inventory = Inventory::default();
+            for skill in convert_c_vec(input.skills) {
+                inventory.grant(Item::Skill(Skill::try_from(*skill as u8).map_err(|_| format!("Invalid skill id {skill}"))?), 1);
+            }
+            for teleporter in convert_c_vec(input.teleporters) {
+                inventory.grant(Item::Teleporter(Teleporter::try_from(*teleporter as u8).map_err(|_| format!("Invalid teleporter id {teleporter}"))?), 1);
+            }
+            for shard in convert_c_vec(input.shards) {
+                inventory.grant(Item::Shard(Shard::try_from(*shard as u8).map_err(|_| format!("Invalid shard id {shard}"))?), 1);
+            }
+
+            let nodes = convert_c_vec(input.nodes).into_iter()
+                .map(|ptr| convert_c_str(*ptr)).collect::<Result<Vec<_>, _>>()?
+                .into_iter().map(str::to_owned).collect::<Vec<_>>();
+
+            let areas = convert_c_str(input.areas)?;
+            let locations = convert_c_str(input.locations)?;
+            let states = convert_c_str(input.states)?;
+            let seed_file = convert_c_str(input.seed_file)?;
+            let universe_settings = UniverseSettings::from_seed(seed_file).unwrap_or_else(|| Err("No settings found in seed".into()))?;
+            let graph = logic::parse_logic(areas, locations, states, &universe_settings, false)?;
+
+            let reached = wotw_seedgen::reach_check(inventory, &graph, seed_file, &nodes)?;
+
+            for node in reached {
+                let identifier = CString::new(node.identifier()).map_err(|err| format!("failed to convert node identifier: {err}"))?;
+                functions.push_reached(data, identifier.into_raw());
+            }
+        }
+        Ok(())
+    })
+}
