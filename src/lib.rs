27ce830f4fd516b84a18912ffd48b59c0366cf21
Timo Schwarzer <timo@schwarzer.dev>
2025-02-14 02:42:41 +0100

    Add function to query states

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,8 @@ use wotw_seedgen::settings::UniverseSettings;
 use wotw_seedgen::logic;
 use wotw_seedgen::Inventory;
 use wotw_seedgen::item::{Item, Skill, Teleporter, Shard, Resource};
+use wotw_seedgen::uber_state::UberStateComparator;
+use wotw_seedgen::world::graph::Node;
 
 #[repr(C)]
 #[derive(Clone, Copy)]
@@ -17,7 +19,7 @@ pub struct RawVec<T> {
 }
 
 #[repr(C)]
-pub struct Input {
+pub struct ReachCheckRequest {
     health: c_float,
     energy: c_float,
     spirit_light: c_int,
@@ -28,26 +30,60 @@ pub struct Input {
     skills: RawVec<c_int>,
     teleporters: RawVec<c_int>,
     shards: RawVec<c_int>,
-    nodes: RawVec<*const c_char>,
+    set_nodes: RawVec<*const c_char>,
+}
+
+#[repr(C)]
+pub struct SeedgenInput {
     areas: *const c_char,
     locations: *const c_char,
     states: *const c_char,
     seed_file: *const c_char,
 }
+
 pub type Data = *const c_void;
+
+trait HandlesErrors {
+    fn push_error(&self, data: Data, string: *const c_char);
+}
+
 #[repr(C)]
-pub struct Functions {
+pub struct ReachCheckFunctions {
     push_error: extern fn(data: Data, string: *const c_char),
     push_reached: extern fn(data: Data, string: *const c_char),
 }
-impl Functions {
+
+impl ReachCheckFunctions {
+    #[inline]
+    fn push_reached(&self, data: Data, string: *const c_char) {
+        (self.push_reached)(data, string)
+    }
+}
+
+impl HandlesErrors for ReachCheckFunctions {
     #[inline]
     fn push_error(&self, data: Data, string: *const c_char) {
         (self.push_error)(data, string)
     }
+}
+
+#[repr(C)]
+pub struct GetStatesFunctions {
+    push_error: extern fn(data: Data, string: *const c_char),
+    push_state: extern fn(data: Data, name: *const c_char, uber_group: i32, uber_state: i32, value: i32, op: i32),
+}
+
+impl GetStatesFunctions {
     #[inline]
-    fn push_reached(&self, data: Data, string: *const c_char) {
-        (self.push_reached)(data, string)
+    fn push_state(&self, data: Data, name: *const c_char, uber_group: i32, uber_state: i32, value: i32, op: i32) {
+        (self.push_state)(data, name, uber_group, uber_state, value, op)
+    }
+}
+
+impl HandlesErrors for GetStatesFunctions {
+    #[inline]
+    fn push_error(&self, data: Data, string: *const c_char) {
+        (self.push_error)(data, string)
     }
 }
 
@@ -58,7 +94,7 @@ pub enum Status {
     Panic,
 }
 
-fn handle_panics<F: FnOnce() -> Result<(), String> + panic::UnwindSafe>(data: Data, functions: &Functions, f: F) -> Status {
+fn handle_panics<F: FnOnce() -> Result<(), String> + panic::UnwindSafe>(data: Data, functions: &dyn HandlesErrors, f: F) -> Status {
     match panic::catch_unwind(f) {
         Ok(result) => match result {
             Ok(()) => Status::Success,
@@ -75,17 +111,22 @@ fn handle_panics<F: FnOnce() -> Result<(), String> + panic::UnwindSafe>(data: Da
 unsafe fn convert_c_str<'a>(ptr: *const c_char) -> Result<&'a str, String> {
     CStr::from_ptr(ptr).to_str().map_err(|err| format!("invalid areas file: {err}"))
 }
+
 unsafe fn convert_c_vec<'a, T>(raw_vec: RawVec<T>) -> &'a [T] {
+    if raw_vec.len == 0 {
+        return &[];
+    }
+
     slice::from_raw_parts(raw_vec.data, raw_vec.len as usize)
 }
 
 #[no_mangle]
-pub extern "C" fn reach_check(input: &Input, data: Data, functions: &Functions) -> Status {
+pub extern "C" fn reach_check(input: &SeedgenInput, reach_check_request: &ReachCheckRequest, data: Data, functions: &ReachCheckFunctions) -> Status {
     handle_panics(data, functions, || {
         unsafe {
-            let inventory = create_inventory(&input)?;
+            let inventory = create_inventory(&reach_check_request)?;
 
-            let nodes = convert_c_vec(input.nodes).into_iter()
+            let set_nodes = convert_c_vec(reach_check_request.set_nodes).into_iter()
                 .map(|ptr| convert_c_str(*ptr)).collect::<Result<Vec<_>, _>>()?
                 .into_iter().map(str::to_owned).collect::<Vec<_>>();
 
@@ -96,7 +137,7 @@ pub extern "C" fn reach_check(input: &Input, data: Data, functions: &Functions) 
             let universe_settings = UniverseSettings::from_seed(seed_file).unwrap_or_else(|| Err("No settings found in seed".into()))?;
             let graph = logic::parse_logic(areas, locations, states, &universe_settings, false)?;
 
-            let reached = wotw_seedgen::reach_check(inventory, &graph, seed_file, &nodes)?;
+            let reached = wotw_seedgen::reach_check(inventory, &graph, seed_file, &set_nodes)?;
 
             for node in reached {
                 let identifier = CString::new(node.identifier()).map_err(|err| format!("failed to convert node identifier: {err}"))?;
@@ -107,27 +148,82 @@ pub extern "C" fn reach_check(input: &Input, data: Data, functions: &Functions) 
     })
 }
 
-unsafe fn create_inventory(input: &Input) -> Result<Inventory, String> {
+#[no_mangle]
+pub extern "C" fn get_states(input: &SeedgenInput, data: Data, functions: &GetStatesFunctions) -> Status {
+    handle_panics(data, functions, || {
+        unsafe {
+            let areas = convert_c_str(input.areas)?;
+            let locations = convert_c_str(input.locations)?;
+            let states = convert_c_str(input.states)?;
+            let seed_file = convert_c_str(input.seed_file)?;
+            let universe_settings = UniverseSettings::from_seed(seed_file).unwrap_or_else(|| Err("No settings found in seed".into()))?;
+            let graph = logic::parse_logic(areas, locations, states, &universe_settings, false)?;
+
+            for node in graph.nodes {
+                match node {
+                    Node::State(state) => {
+                        if state.trigger.is_none() {
+                            continue;
+                        }
+
+                        let identifier = CString::new(state.identifier).map_err(|err| format!("failed to convert state identifier: {err}"))?;
+                        let trigger = state.trigger.unwrap();
+
+                        if let Some(condition) = trigger.condition {
+                            functions.push_state(
+                                data,
+                                identifier.into_raw(),
+                                trigger.identifier.uber_group as i32,
+                                trigger.identifier.uber_id as i32,
+                                condition.value.to_owned() as i32,
+                                match &condition.comparator {
+                                    UberStateComparator::Equals => 2,
+                                    UberStateComparator::Greater => 4,
+                                    UberStateComparator::GreaterOrEquals => 0,
+                                    UberStateComparator::Less => 5,
+                                    UberStateComparator::LessOrEquals => 1,
+                                }
+                            );
+                        } else {
+                            functions.push_state(
+                                data,
+                                identifier.into_raw(),
+                                trigger.identifier.uber_group as i32,
+                                trigger.identifier.uber_id as i32,
+                                1,
+                                2,  // OP = Equal to
+                            );
+                        }
+                    }
+                    _ => {}
+                }
+            }
+        }
+        Ok(())
+    })
+}
+
+unsafe fn create_inventory(reach_check_request: &ReachCheckRequest) -> Result<Inventory, String> {
     let mut inventory = Inventory::default();
 
-    inventory.grant(Item::Resource(Resource::HealthFragment), (input.health / 5.0) as u32);
-    inventory.grant(Item::Resource(Resource::EnergyFragment), (input.energy * 2.0) as u32);
-    inventory.grant(Item::SpiritLight(1), input.spirit_light.try_into().map_err(|_| format!("invalid spirit light amount {}", input.spirit_light))?);
-    inventory.grant(Item::Resource(Resource::GorlekOre), input.gorlek_ore.try_into().map_err(|_| format!("invalid gorlek ore amount {}", input.gorlek_ore))?);
-    inventory.grant(Item::Resource(Resource::Keystone), input.keystone.try_into().map_err(|_| format!("invalid keystone amount {}", input.keystone))?);
-    inventory.grant(Item::Resource(Resource::ShardSlot), input.shard_slot.try_into().map_err(|_| format!("invalid shard slot amount {}", input.shard_slot))?);
+    inventory.grant(Item::Resource(Resource::HealthFragment), (reach_check_request.health / 5.0) as u32);
+    inventory.grant(Item::Resource(Resource::EnergyFragment), (reach_check_request.energy * 2.0) as u32);
+    inventory.grant(Item::SpiritLight(1), reach_check_request.spirit_light.try_into().map_err(|_| format!("invalid spirit light amount {}", reach_check_request.spirit_light))?);
+    inventory.grant(Item::Resource(Resource::GorlekOre), reach_check_request.gorlek_ore.try_into().map_err(|_| format!("invalid gorlek ore amount {}", reach_check_request.gorlek_ore))?);
+    inventory.grant(Item::Resource(Resource::Keystone), reach_check_request.keystone.try_into().map_err(|_| format!("invalid keystone amount {}", reach_check_request.keystone))?);
+    inventory.grant(Item::Resource(Resource::ShardSlot), reach_check_request.shard_slot.try_into().map_err(|_| format!("invalid shard slot amount {}", reach_check_request.shard_slot))?);
 
-    if input.clean_water == 1 {
+    if reach_check_request.clean_water == 1 {
         inventory.grant(Item::Water, 1);
     }
 
-    for skill in convert_c_vec(input.skills) {
+    for skill in convert_c_vec(reach_check_request.skills) {
         inventory.grant(Item::Skill(Skill::try_from(*skill as u8).map_err(|_| format!("Invalid skill id {skill}"))?), 1);
     }
-    for teleporter in convert_c_vec(input.teleporters) {
+    for teleporter in convert_c_vec(reach_check_request.teleporters) {
         inventory.grant(Item::Teleporter(Teleporter::try_from(*teleporter as u8).map_err(|_| format!("Invalid teleporter id {teleporter}"))?), 1);
     }
-    for shard in convert_c_vec(input.shards) {
+    for shard in convert_c_vec(reach_check_request.shards) {
         inventory.grant(Item::Shard(Shard::try_from(*shard as u8).map_err(|_| format!("Invalid shard id {shard}"))?), 1);
     }
 
